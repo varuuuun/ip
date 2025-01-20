39d2edbd3d77a161ecbb7a86246da54121a0939d
varuuuun <varun.s468a@gmail.com>
2025-01-20 18:24:22 +0800

    Change to greet function

--- src/main/java/Bujji.java
+++ src/main/java/Bujji.java
@@ -1,5 +1,9 @@
 public class Bujji {
     public static void main(String[] args) {
+        greet();
+    }
+
+    public static void greet(){
         String botName = "Bujji";
         String lineDivider = "____________________________________________________________";
         String logo = "  ____   _    _       _       _  _____ \n"
@@ -13,6 +17,4 @@ public class Bujji {
         System.out.println("What can I do for you?");
         System.out.println(lineDivider);
     }
-
-    
 }
