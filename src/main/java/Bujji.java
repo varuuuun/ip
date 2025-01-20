6465eef10e0786c4144c1ab9531bca4be88a0833
varuuuun <varun.s468a@gmail.com>
2025-01-20 17:29:44 +0800

    Renamed to Bujji

---
+++ src/main/java/Bujji.java
@@ -0,0 +1,12 @@
+public class Bujji {
+    public static void main(String[] args) {
+        String name = "Bujji";
+        String logo = "  ____   _    _       _       _  _____ \n"
+                + " |  _ \\ | |  | |     | |     | ||_   _|\n"
+                + " | |_) || |  | |     | |     | |  | |  \n"
+                + " |  _ < | |  | | _   | | _   | |  | |  \n"
+                + " | |_) || |__| || |__| || |__| | _| |_ \n"
+                + " |____/  \\____/  \\____/  \\____/ |_____|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
