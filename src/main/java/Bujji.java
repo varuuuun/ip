0f93bb93d425218a70b9067103f0c0a0c45ca055
varuuuun <varun.s468a@gmail.com>
2025-01-21 00:23:32 +0800

    add list and add functionality

--- src/main/java/Bujji.java
+++ src/main/java/Bujji.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bujji {
@@ -5,6 +6,7 @@ public class Bujji {
     String lineDivider;
     String logo;
     Scanner sc;
+    ArrayList<String> taskList;
 
     public Bujji() {
         this.botName = "Bujji";
@@ -16,6 +18,7 @@ public class Bujji {
                 + " | |_) || |__| || |__| || |__| | _| |_ \n"
                 + " |____/  \\____/  \\____/  \\____/ |_____|\n";
         sc = new Scanner(System.in);
+        taskList = new ArrayList<String>(100);
     }
     public void run() {
         greet();
@@ -35,13 +38,33 @@ public class Bujji {
         if (input.equals("bye")) {
             return 0;
         }
-        echo(input);
+
+        if (input.equals("list")) {
+            list();
+        } else {
+            add(input);
+        }
         return 1;
     }
 
     private void echo(String str){
         print(lineDivider);
         print(str);
+        print(lineDivider);
+    }
+
+    private void add(String str){
+        taskList.add(str);
+        print(lineDivider);
+        print("added: " + str);
+        print(lineDivider);
+    }
+
+    private void list(){
+        print(lineDivider);
+        for (int i = 0; i < taskList.size(); i++) {
+            print(i+1 + ". " + taskList.get(i));
+        }
         print(lineDivider);
     }
 
