48ac513487c35d9854fada53e5cf4fffc2f44c0d
varuuuun <varun.s468a@gmail.com>
2025-01-20 18:53:51 +0800

    Add OOP base

--- src/main/java/Bujji.java
+++ src/main/java/Bujji.java
@@ -1,28 +1,37 @@
 public class Bujji {
-    public static void main(String[] args) {
-        greet();
-        exit();
-    }
+    String botName;
+    String lineDivider;
+    String logo;
 
-    public static void greet(){
-        String botName = "Bujji";
-        String lineDivider = "____________________________________________________________";
-        String logo = "  ____   _    _       _       _  _____ \n"
+    public Bujji() {
+        this.botName = "Bujji";
+        this.lineDivider = "____________________________________________________________";
+        this.logo = "  ____   _    _       _       _  _____ \n"
                 + " |  _ \\ | |  | |     | |     | ||_   _|\n"
                 + " | |_) || |  | |     | |     | |  | |  \n"
                 + " |  _ < | |  | | _   | | _   | |  | |  \n"
                 + " | |_) || |__| || |__| || |__| | _| |_ \n"
                 + " |____/  \\____/  \\____/  \\____/ |_____|\n";
-        System.out.println(lineDivider);
-        System.out.println("Hello! I'm " + botName);
-        System.out.println("What can I do for you?");
+    }
+    public void run() {
+        greet();
+        exit();
+    }
+
+    private void greet(){
+        print(lineDivider);
+        print("Hello! I'm " + botName);
+        print("What can I do for you?");
+        print(lineDivider);
+    }
+
+    private void exit(){
+        print("Bye. Hope to see you again soon!");
         System.out.println(lineDivider);
     }
 
-    public static void exit(){
-        String lineDivider = "____________________________________________________________";
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(lineDivider);
+    private void print(String str){
+        System.out.println(str);
     }
 
 }
