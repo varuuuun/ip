860640848f88c83d1b566be6a4e80466849fbedc
varuuuun <varun.s468a@gmail.com>
2025-01-20 18:22:59 +0800

    Add greet message

--- src/main/java/Bujji.java
+++ src/main/java/Bujji.java
@@ -1,12 +1,18 @@
 public class Bujji {
     public static void main(String[] args) {
-        String name = "Bujji";
+        String botName = "Bujji";
+        String lineDivider = "____________________________________________________________";
         String logo = "  ____   _    _       _       _  _____ \n"
                 + " |  _ \\ | |  | |     | |     | ||_   _|\n"
                 + " | |_) || |  | |     | |     | |  | |  \n"
                 + " |  _ < | |  | | _   | | _   | |  | |  \n"
                 + " | |_) || |__| || |__| || |__| | _| |_ \n"
                 + " |____/  \\____/  \\____/  \\____/ |_____|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(lineDivider);
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println(lineDivider);
     }
+
+    
 }
