e4a4aa095c1718e093eaf78bd4b828a1b49ae0cf
varuuuun <varun.s468a@gmail.com>
2025-01-21 14:19:24 +0800

    add Todo, Event and Deadline classes

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,10 @@
+public class Deadline extends Task{
+    public Deadline(String description){
+        super(description);
+    }
+
+    @Override
+    public String toString(){
+        return "[D]" + super.toString();
+    }
+}
