8580cc1c67b59641c3e15398ef4580e6f5771998
varuuuun <varun.s468a@gmail.com>
2025-01-21 13:39:54 +0800

    change string to task with mark & unmark functionality

--- src/main/java/Bujji.java
+++ src/main/java/Bujji.java
@@ -6,7 +6,7 @@ public class Bujji {
     String lineDivider;
     String logo;
     Scanner sc;
-    ArrayList<String> taskList;
+    ArrayList<Task> taskList;
 
     public Bujji() {
         this.botName = "Bujji";
@@ -18,7 +18,7 @@ public class Bujji {
                 + " | |_) || |__| || |__| || |__| | _| |_ \n"
                 + " |____/  \\____/  \\____/  \\____/ |_____|\n";
         sc = new Scanner(System.in);
-        taskList = new ArrayList<String>(100);
+        taskList = new ArrayList<Task>(100);
     }
     public void run() {
         greet();
@@ -41,6 +41,12 @@ public class Bujji {
 
         if (input.equals("list")) {
             list();
+        } else if (input.startsWith("mark")){
+            //TODO: Error checks
+            mark(Integer.parseInt(input.split(" ")[1]) - 1);
+        } else if (input.startsWith("unmark")){
+            //TODO: Error checks
+            unmark(Integer.parseInt(input.split(" ")[1]) - 1);
         } else {
             add(input);
         }
@@ -54,9 +60,25 @@ public class Bujji {
     }
 
     private void add(String str){
-        taskList.add(str);
+        taskList.add(new Task(str));
         print(lineDivider);
         print("added: " + str);
+        print(lineDivider);
+    }
+
+    private void mark(int i){
+        Task t = taskList.get(i);
+        t.setDone();
+        print(lineDivider);
+        print("Nice! I've marked this task as done: \n\t" + t);
+        print(lineDivider);
+    }
+
+    private void unmark(int i){
+        Task t = taskList.get(i);
+        t.setNotDone();
+        print(lineDivider);
+        print("OK, I've marked this task as not done yet: \n\t" + t);
         print(lineDivider);
     }
 
