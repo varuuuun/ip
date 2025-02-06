d1e886acff6438f6f722d6507b471cee5e1ad8df
varuuuun <varun.s468a@gmail.com>
2025-02-07 01:26:55 +0800

    Add controllers for JavaFx Gui

---
+++ src/main/java/mitri/ui/DialogBox.java
@@ -0,0 +1,59 @@
+package mitri.ui;
+
+import java.io.IOException;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.fxml.FXMLLoader;
+
+public class DialogBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialogBox(String s, Image i) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        text = new Label(s);
+        displayPicture = new ImageView(i);
+        //Styling the dialog box
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        this.setAlignment(Pos.TOP_RIGHT);
+
+        this.getChildren().addAll(text, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(String s, Image i) {
+        return new DialogBox(s, i);
+    }
+
+    public static DialogBox getMitriDialog(String s, Image i) {
+        var db = new DialogBox(s, i);
+        db.flip();
+        return db;
+    }
+}
---
+++ src/main/java/mitri/ui/Gui.java
@@ -0,0 +1,98 @@
+package mitri.ui;
+
+import java.io.IOException;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import mitri.util.Parser;
+
+public class Gui extends Ui {
+
+    @javafx.fxml.FXML
+    private ScrollPane scrollPane;
+    @javafx.fxml.FXML
+    private VBox dialogContainer;
+    @javafx.fxml.FXML
+    private TextField userInput;
+    @javafx.fxml.FXML
+    private Button sendButton;
+    private Scene scene;
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private Image mitriImage = new Image(this.getClass().getResourceAsStream("/images/chatbot.png"));
+    private Parser parser;
+
+    public Gui() {
+        this.dialogContainer = new VBox();
+    }
+
+    public Gui(Parser parser) {
+        this.parser = parser;
+    }
+
+    public void start(Stage stage){
+        //Setting up required components
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource("/view/Gui.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setParser(Parser parser) {
+        this.parser = parser;
+    }
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        displayOutput("Hello! I'm Mitri\nWhat can I do for you?");
+    }
+
+    @FXML
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        dialogContainer.getChildren().addAll(
+                mitri.ui.DialogBox.getUserDialog(userText, userImage)
+        );
+        userInput.clear();
+        dialogContainer.getChildren().addAll(
+                mitri.ui.DialogBox.getMitriDialog(parser.processInput(userText), mitriImage)
+        );
+
+    }
+
+    @FXML
+    public void displayOutput(String output){
+        dialogContainer.getChildren().addAll(
+                mitri.ui.DialogBox.getMitriDialog(output, mitriImage)
+        );
+    }
+
+    @FXML
+    public void displayError(String error){
+        dialogContainer.getChildren().addAll(
+                mitri.ui.DialogBox.getMitriDialog("Error: " + error, mitriImage)
+        );
+    }
+
+    public String getError(String error){
+        return "Error: " + error;
+    }
+
+}
