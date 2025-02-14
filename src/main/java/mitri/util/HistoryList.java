abc8813f7088eadab04d1621ba48910074553d95
varuuuun <varun.s468a@gmail.com>
2025-02-14 09:48:30 +0800

    Add list to store history of change-causing commands

---
+++ src/main/java/mitri/util/HistoryList.java
@@ -0,0 +1,55 @@
+package mitri.util;
+
+import java.util.ArrayList;
+
+/**
+ * Represents Task list.
+ */
+public class HistoryList{
+    private ArrayList<String> commands;
+
+    /**
+     * Initialises history list.
+     */
+    public HistoryList() {
+        commands = new ArrayList<String>();
+    }
+
+    /**
+     * Removes command at top of list.
+     *
+     * @return Removed command.
+     */
+    public String remove() throws IllegalArgumentException{
+        if (commands.isEmpty()) {
+            throw new IllegalArgumentException("There is no command to undo");
+        }
+        return commands.remove(0);
+    }
+
+    /**
+     * Adds command to list.
+     *
+     * @param command command add to list.
+     */
+    public void add(String command) {
+        commands.add(0, command);
+    }
+
+    public void addFromFile(String command) {
+        commands.add(command);
+    }
+
+    /**
+     * Returns current size of list.
+     *
+     * @return Size of list.
+     */
+    public int size() {
+        return commands.size();
+    }
+
+    public String get(int index) {
+        return commands.get(index);
+    }
+}
